4ce639cbd35171e2a98cb1ac9eb7e72e00cf846e
InseopSeo <seo000408@gmail.com>
2023-10-26 18:10:34 +0900

    modified

--- Lecture_06_HTTP_1_1_Source_Code_v3/01http-web-server.go
+++ Lecture_06_HTTP_1_1_Source_Code_v3/01http-web-server.go
@@ -3,12 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 type MyHttpHandler struct{}
 
+func (h MyHttpHandler) print_http_request_detail(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("::Client address   : ", strings.Split(r.RemoteAddr, ":")[0])
+	fmt.Println("::Client port      : ", strings.Split(r.RemoteAddr, ":")[1])
+	fmt.Println("::Request command  : ", r.Method)
+	fmt.Println("::Request line     : ", r.RequestURI)
+	fmt.Println("::Request path     : ", r.URL.Path)
+	fmt.Println("::Request version  : ", r.Proto)
+}
+
+func (h MyHttpHandler) send_http_response_header() {
+
+}
+
 func (h MyHttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, World!")
+}
+
+func (h MyHttpHandler) simpleCalc(para1, para2 int) int {
+	return para1 * para2
+}
+
+func (h MyHttpHandler) parameter_retrieval(msg string) {
+	//var result []int
+
 }
 
 func main() {
