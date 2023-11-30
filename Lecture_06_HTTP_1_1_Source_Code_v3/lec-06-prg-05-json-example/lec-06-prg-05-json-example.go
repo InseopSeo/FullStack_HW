fa21f8b6c3cb9dccd54c96396b1c6c5b8beabef4
InseopSeo <seo000408@gmail.com>
2023-11-30 17:56:33 +0900

    update 4,5,6

--- Lecture_06_HTTP_1_1_Source_Code_v3/lec-06-prg-05-json-example/lec-06-prg-05-json-example.go
+++ Lecture_06_HTTP_1_1_Source_Code_v3/lec-06-prg-05-json-example/lec-06-prg-05-json-example.go
@@ -3,12 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"os"
 )
 
 func main() {
-
-	superHeroes := map[string]interface{}{
+	superHeroes_Source := map[string]interface{}{
 		"squadName":  "Super hero squad",
 		"homeTown":   "Metro City",
 		"formed":     2016,
@@ -50,24 +48,11 @@ func main() {
 		},
 	}
 
-	fmt.Println(superHeroes["homeTown"])
-	fmt.Println(superHeroes["active"])
-	fmt.Println(superHeroes["members"].([]map[string]interface{})[1]["powers"].([]string)[2])
-
-	file, err := os.Create("lec-06-prg-04-json-example.json")
+	superHeroesJSON, err := json.MarshalIndent(superHeroes_Source, "", "    ")
 	if err != nil {
-		fmt.Println("error:", err)
+		fmt.Println("Error:", err)
 		return
 	}
 
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "\t")
-	err = encoder.Encode(superHeroes)
-	if err != nil {
-		fmt.Println("error:", err)
-		return
-	}
-
+	fmt.Println(string(superHeroesJSON))
 }
